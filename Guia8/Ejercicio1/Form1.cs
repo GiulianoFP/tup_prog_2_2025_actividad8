6a20ff7fb6d5e525781424fd315ca5b5546d9c4a
GiulianoFP <Giuliano15150@gmail.com>
2025-09-25 17:28:00 -0300

    .

---
+++ Guia8/Ejercicio1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ejercicio1.Models;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        List<Vehiculo> lista=new List<Vehiculo>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            string path = "C:\\Users\\Alumno\\source\\repos\\tup_prog_2_2025_actividad8\\Guia8\\Ejercicio1\\bin\\Debug\\Vehiculos.csv";
+            string[] lineas = File.ReadAllLines(path);
+
+
+            foreach (string line in lineas)
+            {
+                string[] s = line.Split(';');
+                txtMostrar.Text += s[0] + " " + s[1] + "\r\n";
+            }
+            OpenFileDialog ofd=new OpenFileDialog();
+            ofd.Filter = "Archivo CSV|*.csv|todos los " +
+                "archivos|*.*";
+            if (ofd.ShowDialog()==DialogResult.OK)
+            {
+                string name = ofd.FileName;
+                FileStream fs = null;
+                StreamReader sr = null;
+
+                try
+                {
+                    fs = new FileStream(name,
+                       FileMode.Open, FileAccess.Read);
+
+                    sr = new StreamReader(fs);
+
+
+                    sr.ReadLine();
+                    while (sr.EndOfStream != true)
+                    {
+                        string cadena = sr.ReadLine().Trim();
+                        string[] separator = cadena.Split(';');
+
+                        string patente = separator[0];
+                        double importe = Convert.ToDouble(separator[1]);
+
+                        Vehiculo vehiculo = new Vehiculo(patente, importe);
+                        lista.Add(vehiculo);
+
+                    }
+                }
+                catch
+                {
+
+                    MessageBox.ShowDialog("Error");
+                    MessageBox.Text
+                    
+                }
+                finally
+                {
+
+                }
+
+                foreach (Vehiculo v in lista)
+                {
+                    txtMostrar.Text = v.ToString();
+                }
+
+            }
+        }
+    }
+}
---
+++ Guia8/Ejercicio1/Models/Vehiculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    public class Vehiculo
+    {
+        public string Patente;
+        public double Importe;
+
+        public Vehiculo(string patente, double importe)
+        {
+            Patente = patente;
+            Importe = importe;
+        }
+
+        public override string ToString()
+        {
+            return $"Patente:{Patente}  Importe:${Importe}";
+        }
+    }
+}
